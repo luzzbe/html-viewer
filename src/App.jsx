22a770a5ac6b10f4c73369daa344304a0ba058b8
Timothée Lebourdais <timothee.leb@icloud.com>
2024-04-21 13:01:36 +0200

    Correction de la mise en page pour les petits écrans dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div>
-      <div className="flex md:hidden h-screen justify-center items-center">
+      <div className="flex md:hidden justify-center mt-20">
         <div className="card w-96 bg-neutral text-neutral-content">
           <div className="card-body items-center text-center">
             <h2 className="card-title">Taille d&apos;écran non supportée</h2>
