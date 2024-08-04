26b7a14359616e323d22415f8b987a32b52b3a85
Timothée Lebourdais <timothee.leb@icloud.com>
2024-08-04 08:31:46 +0200

    add titleon iframe

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 export default function Preview({ content }) {
   return (
     <iframe
+      title="preview"
       srcDoc={content}
       className="bg-white w-full h-full border-0"
       sandbox=""
