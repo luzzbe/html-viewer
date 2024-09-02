df59b84936945e4d5090cfcee1e0b1d85b070082
Timothée Lebourdais <timothee.leb@icloud.com>
2024-09-02 18:59:17 +0200

    Open links in new tab

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
 
 export default function Preview({ content }) {
+  const openLinksInNewTab = (e) => {
+    const iframe = e.target;
+    const iframeDocument = iframe.contentWindow.document;
+    const links = iframeDocument.querySelectorAll("a");
+    links.forEach((link) => link.setAttribute("target", "_blank"));
+  };
+
   return (
     <iframe
       title="preview"
       srcDoc={content}
       className="bg-white w-full h-full border-0"
-      sandbox=""
+      sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin"
+      onLoad={openLinksInNewTab}
     />
   );
 }
